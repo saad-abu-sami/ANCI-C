0045c7306d1f2e5d5eec7f888f1264fde82a8b14
Saad Abu Sami <60351152+saad-abu-sami@users.noreply.github.com>
2021-05-26 14:24:14 +0600

    Alhamdulillah

--- test.c
+++ test.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
 	int n, row, col;
 	printf("Enter N = ");
 	scanf("%d", &n);
-	for (row = 1; row <= n; row++){
-		for(col = 1; col <= row; col++){
-			printf("%d ", col);
+	for(row=1 ; row<=n ; row++){
+		for(col=1 ; col<=n-row ; col++){
+			printf(" ");
+		}
+		for(col=1 ; col<=2*row-1 ; col++){
+			printf("%c",row+96);
+		}
+		printf("\n");
+	}
+	for(row = n-1 ; row >= 1 ; row--){
+		for(col = 1 ; col <= n-row ; col++){
+			printf(" ");
+		}
+		for(col=1 ; col<=2*row-1 ; col++){
+			// on first row
+			// (2*5-1)=9, so 9 sign print on first row
+			printf("%c",row+96);
 		}
 		printf("\n");
 	}
